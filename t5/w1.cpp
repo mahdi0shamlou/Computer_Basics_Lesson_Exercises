8f3515c1cf343261cd8a8379e411db8e61c63ef5
Mahdi Shamlou <shamlouprogrammer@gmail.com>
2025-06-08 10:14:44 +0000

    Update t5 and test it

---
+++ t5/w1.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+double calculateSeriesSum(double x, int terms) {
+    double sum = 0.0;
+    double term = x; // جمله اول: x
+    
+    for (int i = 1; i <= terms; i++) {
+        // اضافه کردن جمله فعلی به مجموع
+        if (i % 2 == 1) // جملات فرد: مثبت
+            sum += term;
+        else // جملات زوج: منفی
+            sum -= term;
+        
+        // محاسبه جمله بعدی با استفاده از رابطه بازگشتی
+        // term_next = term * (x^2) / [(2i)(2i+1)]
+        int n = 2 * i;
+        term = term * (x * x) / (n * (n + 1));
+    }
+    return sum;
+}
+
+int main() {
+    double x;
+    int terms;
+    
+    cout << "Enter the value of x: ";
+    cin >> x;
+    
+    cout << "Enter the number of terms: ";
+    cin >> terms;
+    
+    double sum = calculateSeriesSum(x, terms);
+    
+    cout << fixed << setprecision(8);
+    cout << "Sum of the series: " << sum << endl;
+
+    
+    return 0;
+}
