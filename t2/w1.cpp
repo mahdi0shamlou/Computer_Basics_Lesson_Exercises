63cf8fcaa2e81a993a0129d2585983b0ac7f45d4
Mahdi Shamlou <shamlouprogrammer@gmail.com>
2025-06-08 06:26:19 +0000

    Update t2 and test it its okay

--- t2/w1.cpp
+++ t2/w1.cpp
@@ -2,8 +2,8 @@
 using namespace std;
 
 int main() {
-    const int BIG_SIZE = 50;
-    const int SMALL_SIZE = 5;
+    const int BIG_SIZE = 5;
+    const int SMALL_SIZE = 2;
     int bigArray[BIG_SIZE];
     int smallArray[SMALL_SIZE];
 
@@ -17,6 +17,32 @@ int main() {
         cin >> smallArray[i];
     }
 
+    bool is_in = true;
+
+    for (int i = 0; i < SMALL_SIZE; i++) {
+        int index_should_search_in_big_array = smallArray[i];
+        bool index_should_search_in_big_array_not_exsit_in_big_array = true;
+
+        for (int j = 0; j < BIG_SIZE; j++) {
+                if(index_should_search_in_big_array == bigArray[j]){
+                    index_should_search_in_big_array_not_exsit_in_big_array = false;
+                    break;
+                }
+        }
+
+        if(index_should_search_in_big_array_not_exsit_in_big_array){
+            is_in = false;
+            break;
+        }
+
+    }
+    
+    if(!is_in) {
+        cout << "Array is not exsit in first array";
+    }
+    else{
+        cout << "Array is exsit in first array";
+    }
 
 
     return 0;
